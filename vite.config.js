b1ce0ce2e709e304ffeb07588ed32dfb2daf0c50
ClickyMcTypey <reyrandel@gmail.com>
2026-06-09 22:54:19 +0800

    vite update

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         sourcemap: true,
+        emptyOutDir: true,
         rollupOptions: {
-            input: 'index.html',
+            input: {
+                'athn-resource-utils': 'src/main.js'
+            },
             output: {
-                entryFileNames: 'assets/athn-resource-utils.[hash].js',
+                entryFileNames: 'assets/[name].[hash].js',
                 chunkFileNames: 'assets/[name].[hash].js',
                 assetFileNames: 'assets/[name].[hash][extname]'
             }
